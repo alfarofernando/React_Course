aa770aca63778a03b585f1c7f05a9b8b8ffb25a4
AlfaDevelooper <alfarofernando@live.com>
2024-08-19 14:17:56 -0300

    Reusabilidad de componentes

--- reactcourse/src/App.jsx
+++ reactcourse/src/App.jsx
@@ -2,7 +2,12 @@ import Hello from './components/Hello';
 
 function App() {
   return (
+    // la caracteristica mas importante de React son la utilizacion de componentes
+    // trozos de HTML y logica que se llaman logrando una abstraccion de la complejidad de las mismas
     <div className="App">
+      <Hello />
+      <Hello />
+      <Hello />
       <Hello />
     </div>
   );
--- reactcourse/src/components/Hello.jsx
+++ reactcourse/src/components/Hello.jsx
@@ -1,19 +1,7 @@
-
-const name = "Fer";
-
-function displayMessage() {
-    return "wow!";
-}
-
 function Hello() {
-    // JSX mezcla HTML con JS como si de PHP se tratase.
-    // por lo que se permite la utilizacion de variables y de funciones dentro de llaves dentro del HTML 
-    // en un return solo puede una sola etiqueta padre por lo que si se utilizara mas de una etiqueta deben estar dentro 
-    //de una unica etiqueta padre.
     return (
         <div>
-            <h1>Hello there, i am {name} learning React</h1>
-            <h2>{displayMessage()}</h2>
+            <h1>Hello World!</h1>
         </div>
     );
 }
