bbd8b1dc60cfc6a3682a9f7357501d781eabd7c4
AlfaDevelooper <alfarofernando@live.com>
2024-08-19 14:07:24 -0300

    Little explaination about JSX in React

--- reactcourse/src/components/Hello.jsx
+++ reactcourse/src/components/Hello.jsx
@@ -1,5 +1,21 @@
+
+const name = "Fer";
+
+function displayMessage() {
+    return "wow!";
+}
+
 function Hello() {
-    return <h1>Hello from a component :D</h1>; // cuando el return esta en la msima linea no hace falta poner () por lo que luego de agregar lo que queremos retornar solamente debemos cerrar el return con ;
+    // JSX mezcla HTML con JS como si de PHP se tratase.
+    // por lo que se permite la utilizacion de variables y de funciones dentro de llaves dentro del HTML 
+    // en un return solo puede una sola etiqueta padre por lo que si se utilizara mas de una etiqueta deben estar dentro 
+    //de una unica etiqueta padre.
+    return (
+        <div>
+            <h1>Hello there, i am {name} learning React</h1>
+            <h2>{displayMessage()}</h2>
+        </div>
+    );
 }
 
 export default Hello;
