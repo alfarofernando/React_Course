53f5d618ef354c6978ef8b8755b5d3d9e0a3884f
AlfaDevelooper <alfarofernando@live.com>
2024-08-19 14:42:36 -0300

    What are Props

--- reactcourse/src/App.jsx
+++ reactcourse/src/App.jsx
@@ -1,14 +1,19 @@
 import Hello from './components/Hello';
 
 function App() {
+  // asi como las etiquetas HTML aceptan atributos como style , name, class, etc 
+  // las etiquetas de componentes pueden aceptar propiedades o props
+  // que pueden modificar la vista de los componentes dinamicamente de acuerdo a las propiedades que estos adquieren
+  // la posicion de las props en la implementacion del componente no modifica la logica del mismo
+  // por lo que en est eejemplo colocar message antes que name no modifica el output
   return (
-    // la caracteristica mas importante de React son la utilizacion de componentes
-    // trozos de HTML y logica que se llaman logrando una abstraccion de la complejidad de las mismas
+  
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Fer" message="Hi There , " />
+      <Hello name="Lean" message="Hello There , "/>
+      <Hello  message="Long time no see , "  name="Denu"/>
+      <Hello name="Vicky" message="I have been waiting for you , " />
+      <Hello name="Luquis" message="It has been a while , " />
     </div>
   );
 }
--- reactcourse/src/components/Hello.jsx
+++ reactcourse/src/components/Hello.jsx
@@ -1,7 +1,10 @@
-function Hello() {
+function Hello(props) {
+// como cualquier otra funcion somos capaces de pasarle parametros a la misma 
+// los parametros son OBJETOS los cuales no pueden renderizarce directamente en html sino que deben ser llamados
+// por lo que la sintaxis seria   nombreObjetoParametroFuncion .(punto) nombrePropiedadEtiquetaComponente
     return (
         <div>
-            <h1>Hello World!</h1>
+            <h1>{props.message} { props.name }</h1>
         </div>
     );
 }
