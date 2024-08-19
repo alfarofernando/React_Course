e12dffa376322ede389c5e778948dc7e6a21776c
AlfaDevelooper <alfarofernando@live.com>
2024-08-19 10:03:44 -0300

    Creating a component in React

---
+++ reactcourse/src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+    return <h1>Hello from a component :D</h1>; // cuando el return esta en la msima linea no hace falta poner () por lo que luego de agregar lo que queremos retornar solamente debemos cerrar el return con ;
+}
+
+export default Hello;
