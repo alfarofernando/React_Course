63ebc2e07fdccd5b14f4dabb783419e5466ee9f3
alfarofernando <alfarofernando@live.com>
2024-08-20 05:51:34 -0300

    Conditional rendering using element variables

--- reactcourse/src/components/ConditionalComponent2.jsx
+++ reactcourse/src/components/ConditionalComponent2.jsx
@@ -4,10 +4,18 @@ import Code2 from "./Code2";
 export default function ConditionalComponent2() {
 
     const display = true;
+//como no es una buena practica el tener dos o mas returns desntro
+//de una estuctura de control lo que se hace es 
+// crear un elemento variable al cual no se le asigna ningun valor
+// y dependioendo de la condicion a esta variable se le asiganara como valor
+// un componente u otro 
+    let message;
 
     if (display) {
-        return <Code />
+        message = <Code/> ;
     } else {
-        return <Code2 />
+        message = <Code2/> ;
     }
+
+    return message;
 }
