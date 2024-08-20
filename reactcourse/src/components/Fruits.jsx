acc84070814df8ef4cb1760e000cd823663849e9
alfarofernando <alfarofernando@live.com>
2024-08-20 04:55:50 -0300

    Rendering array of objects

--- reactcourse/src/components/Fruits.jsx
+++ reactcourse/src/components/Fruits.jsx
@@ -1,22 +1,45 @@
 export default function Fruits() {
-    //para renderisar un array primero debemos crearlo  
-    const fruits = ["apple", "mango", "banana", "orange", "pineapple"];
-    // luego debemos llamar al metodo map del array para poder recorrerlo 
-    // los parametros del array sera el mismo nombre del array en singular para
-    // indicar que sera un solo elemento de la lista de elementos 
-    // el cual retornara mediante una funcion flecha algo
-    // en este caso devolveremos un h1 con el contenido directo de el elemento actual
-    // en la iteracion del metodo map aplicado al array
-    // tambien hace falta colocar una key para indicar la posicion unica
-    // del elemento que se retorna 
-
-
+// para renderizar un array de objetos primero creamos los mismos
+    const fruits = [
+        {
+            name: "apple",
+            description: "apple from argentina",
+            price: 3.5,
+        },
+        {
+            name:"mango",
+            description:"mango from chile",
+            price: 2.9,
+        },
+        {
+            name:"banana",
+            description:"banana from ecuador",
+            price:5,
+        },
+        {
+            name:"orange",
+            description:"orange from bolivia",
+            price: 2,
+        },
+        {
+            name:"pineapple",
+            description:"pineapple from brazil",
+            price:4.8,
+        },
+    ];
+// ahora para renderizar nuestro array de objetos
+// en lugar de llamar directamente al objeto debemos llamar al elemento del objeto
+// seguido de el atributo que queremos mostrar del mismo en las iteraciones
+// tambien usaremos como unique key el nombre de la fruta en cuestion ya que los mismos
+// no se repiten. aunque tambien podemos usar como unique key la letra i de index
     return (
         <div>
             <ul>
                 {fruits.map(fruit => (
-                    <li key={fruit}>
-                        {fruit}
+                    <li key={fruit.name}>
+                        <p>Fruit: {fruit.name}</p> 
+                        <p>Description: {fruit.description}</p> 
+                        <p>Price: ${fruit.price}</p>
                     </li>
                 ))}
             </ul>
