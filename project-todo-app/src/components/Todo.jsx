33db1f6f65556bc2cfabd7aeaa40ee2d4589fdbd
alfarofernando <alfarofernando@live.com>
2024-08-22 06:06:26 -0300

    submiting todo handling

--- project-todo-app/src/components/Todo.jsx
+++ project-todo-app/src/components/Todo.jsx
@@ -1,18 +1,45 @@
 import { useState } from "react";
 //importmaos el hook de react
 
-export default function Todo(){
+export default function Todo() {
 
-    //creamos el estado todo para guardar las tareas de la aplicacion
-    const [todo,setTodo] = useState("");
-    return(
+    //creamos el estado todo para guardar el valor ingresado por el input
+    const [toDo, setTodo] = useState("");
+
+    // creamos el estado como array para formar una lista
+    // con los valores que recoje el estado toDo
+    const [toDoList, setToDoList] = useState([]);
+
+    //creamos funcion para manejar el submit del formulario
+    //al cual añadimos un console log para verificar la correcta entrada de datos
+    //prescindiendo del parrafo en el return
+    function handleSubmit(e) {
+        //previene recarga de pagina
+        e.preventDefault();
+        //console log con contenido de variable toDo
+        console.log(toDo);
+        //setea el array de strings guardando los datos que se van ingresando por el input
+        // en el array toDoList
+        setToDoList([...toDoList , toDo]);
+    }
+
+    return (
         <div>
-  {/*colocamos momentaneamente un parrafo para verificar la entrada de datos*/}
+            {/*colocamos momentaneamente un parrafo para verificar la entrada de datos
             <p>{todo}</p>
-            <form>
-                <input onChange={(e)=>setTodo(e.target.value)} value={todo} type="text"/>
+    Al tener ya la verificacion en el submit del formulario prescindimos del parrafo*/}
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input
+                    onChange={(e) => setTodo(e.target.value)}
+                    value={toDo}
+                    type="text"
+                />
                 <button type="submit">Add</button>
             </form>
+        {
+        /*console log verificando el array de strings*/
+        console.log(toDoList)
+        }
         </div>
     );
 }
