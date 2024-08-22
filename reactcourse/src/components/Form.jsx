c7f63c1559010396200b0206a537e5007ce8a1b4
alfarofernando <alfarofernando@live.com>
2024-08-22 04:06:30 -0300

    handlng multiple inputs

--- reactcourse/src/components/Form.jsx
+++ reactcourse/src/components/Form.jsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
 
 export default function Form(){
-
-    //en react el value de los forms deben ser manejados por estados
-    //por lo que usaremos el atributo onChange en el input para manejar los cambios
- /*    function handleChange(e){
-        console.log(e.target.value);
-    } */
-    // y crearemos un estado para cambiar el value del input
-    const [name,setName] = useState("");
-    /* 
-    function demo(e){
-        return handleChange(e);
-    }
- */
-    //finalmente simplificamos el codigo para que dentro del atributo onChange 
-    //con una funcion flecha con el objeto evento de parametro 
-    //setiemos el nombre llamando a ese objeto con target.value y lo pasemos 
-    //a la variable del estado name
+    //para manejar eventos en multiples inputs lo que se puede hacer es 
+    // crear un objeto en el state con los corchetes e indicando
+    //las propiedades que va a poseer dicho objeto
+    const [name,setName] = useState({firstName:"" , lastName:""});
+    // y para capturar la informacion que ingresa por el form
+    // hay que tener en cuenta que setName ahora va a recibir valores para dos propiedades
+    // diferentes de un mismo objeto por lo que hay que indicarle en que propiedad 
+    // se va a almacenar el input
+    // tambien para mantener lo capturado anteriormente por uno u otro input 
+    // se coloca entre llaves ...objeto, propiedadAModificar
+    // para indicarle al estado 
+    //que no importa las propiedades que tengo el objeto en cuestion 
+    // solo vamos a estar modificando una propiedad en concreto
+    // esto hace que no tengamos que llamar a todas las pripiedades para cambiar una sola
     return (
         <div>
+
+            <h2>Nombre ingresado: {name.firstName}</h2>
+            <h2>Apellido ingresado: {name.lastName}</h2>
+            <hr/>
             <form action="">
-                <input 
-                    onChange={(e)=>setName(e.target.value)} 
+               Nombre: <input 
+                    onChange={
+                        (e)=>setName(
+                            {...name , firstName:e.target.value}
+                            
+                        )} 
                     type="text"
-                    value={name} />
+                    value={name.firstName}
+                /><br/>
+                Apellido<input
+                    onChange={
+                        (e)=>setName(
+                            {...name, lastName:e.target.value}
+                        )}
+                    type="text"
+                    value={name.lastName}
+                />
             </form>
         </div>
     );
