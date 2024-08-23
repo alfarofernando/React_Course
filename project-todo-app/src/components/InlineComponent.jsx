c60454a4a491963ce6e43294dcb8b0096e379096
AlfaDevelooper <alfarofernando@live.com>
2024-08-23 08:36:22 -0300

    css stylesheet to style components

--- project-todo-app/src/components/InlineComponent.jsx
+++ project-todo-app/src/components/InlineComponent.jsx
@@ -1,11 +1,16 @@
-const style = { color: "blue", fontSize: "140px" }
+import '../css/style.css';   
+//una manera de darle estilos personalizados a nuestro react es medianto la importacion de archivos css
+//
 //otra manera es creando una variable que contenga el objeto de estilos oara llamarlo desde la etiqueta mas abajo
+// const style = { color: "blue", fontSize: "140px" }
 export default function InlineComponent() {
     return(
         <div>
            {/*  una manera de aplicar efectos de css en React es medianto el uso de estilos en linea; */}
             {/* <h1 style={{ color: "blue", fontSize: "140px" }} > */}
-             <h1 style={style} >
+            {/* <h1 style={style} > */}
+            {/*             cuando trabajamos con archivos css llamamos a los estilos definidos mediante el uso de className */}
+            <h1 className='header' >
                 Inline Component
              </h1>
         </div>
