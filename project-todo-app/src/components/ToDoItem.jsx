eff7654256a9317cc2ef507c866ef3e95160e95c
alfarofernando <alfarofernando@live.com>
2024-08-24 05:16:14 -0300

    styling to do list and item

--- project-todo-app/src/components/ToDoItem.jsx
+++ project-todo-app/src/components/ToDoItem.jsx
@@ -4,9 +4,17 @@ export default function ToDoItem({item}) {
     
     return (
         <div className={styles.item}>
+            
             <div className={styles.itemname}>
                 {item}
+
+                <span>
+                    <button className={styles.itembutton}> X </button>
+                </span>
             </div>
+
+            
+
             <hr  className={styles.line} />
         </div>
     );
