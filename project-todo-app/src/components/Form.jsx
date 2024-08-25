c4bd24362bf25486a4dbbc193cc8b7112f76f7ec
AlfaDevelooper <alfarofernando@live.com>
2024-08-25 12:16:35 -0300

    form validation for duplicates and empty input

--- project-todo-app/src/components/Form.jsx
+++ project-todo-app/src/components/Form.jsx
@@ -1,33 +1,50 @@
 import { useState } from "react";
-import styles from './Form.module.css';
+import styles from "./Form.module.css";
 
-export default function Form({ setToDoList, toDoList }) { // Recibe las props desde el componente padre
-    
-    //estado que guarda un array de objetos con propiedades nombre y si estan completas o no
-    const [toDo, setTodo] = useState({name:"",done:false});
-    
+export default function Form({ setToDoList, toDoList }) {
+  // Recibe las props desde el componente padre
 
-    function handleSubmit(e) {
-        e.preventDefault(); // Previene que el formulario recargue la página al hacer submit
-        console.log(toDo);  // Muestra en consola el valor actual del input
-        setToDoList([...toDoList , toDo]); // Agrega el nuevo ítem a la lista
-        setTodo({name:"",done:false}); // Limpia el input después de añadir la tarea
+  //estado que guarda un array de objetos con propiedades nombre y si estan completas o no
+  const [toDo, setTodo] = useState({ name: "", done: false });
+
+  function handleSubmit(e) {
+    e.preventDefault(); // Previene que el formulario recargue la página al hacer submit
+    console.log(toDo); // Muestra en consola el valor actual del input
+
+    if (toDo.name === "") {
+      alert("Cant input an empty task");
+      return;
     }
 
-    return (
-        <>
-            <form className={styles.todoform} onSubmit={handleSubmit}>
-                <input
-                    className={styles.todoinput}
-                    onChange={(e) => setTodo({ name: e.target.value , done:false }) /* camputarmos el valor del input en la propiedad name del objeto */} 
-                    value={toDo.name}
-                    type="text"
-                    placeholder="...Enter Task To Do..."
-                />
-                <button
-                    className={styles.todobutton}
-                    type="submit">Add</button>
-            </form>
-        </>
-    );
+    if (toDoList.some((todo) => todo.name === toDo.name)) {
+      alert("Cant duplicate tasks");
+      return;
+    }
+
+    setToDoList([...toDoList, toDo]); // Agrega el nuevo ítem a la lista
+    setTodo({ name: "", done: false }); // Limpia el input después de añadir la tarea
+  }
+
+  return (
+    <>
+      <form className={styles.todoform} onSubmit={handleSubmit}>
+        <input
+          className={styles.todoinput}
+          onChange={
+            (e) =>
+              setTodo({
+                name: e.target.value,
+                done: false,
+              }) /* camputarmos el valor del input en la propiedad name del objeto */
+          }
+          value={toDo.name}
+          type="text"
+          placeholder="...Enter Task To Do..."
+        />
+        <button className={styles.todobutton} type="submit">
+          Add
+        </button>
+      </form>
+    </>
+  );
 }
