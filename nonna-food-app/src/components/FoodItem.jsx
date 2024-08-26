1f38216702775cc3f00ce7ed401cfc36372f85f1
alfarofernando <alfarofernando@live.com>
2024-08-26 06:49:39 -0300

    food list styling

--- nonna-food-app/src/components/FoodItem.jsx
+++ nonna-food-app/src/components/FoodItem.jsx
@@ -1,9 +1,14 @@
-export default function FoodItem({food}){
-    return(
-        <div>
-            <img src={food.image} alt="" />
-            <h1>{food.title}</h1>
-            <button>View Recipe</button>
+import styles from "./FoodItem.module.css";
+
+export default function FoodItem({ food }) {
+    return (
+        <div className={styles.cardContainer}>
+            <img className={styles.cardImage} src={food.image} alt="" />
+            <div className={styles.cardContent}>
+                <p className={styles.cardTitle}>{food.title}</p>
+
+                <button className={styles.cardButton} >View Recipe</button>
+            </div>
         </div>
     );
 }
