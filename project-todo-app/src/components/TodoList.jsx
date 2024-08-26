93a9dffd18a7e634d5f0d004a0d2abcbc128a53d
alfarofernando <alfarofernando@live.com>
2024-08-25 23:45:16 -0300

    finish todoapp & start of Nonna's Recipes food application

--- project-todo-app/src/components/TodoList.jsx
+++ project-todo-app/src/components/TodoList.jsx
@@ -1,13 +1,22 @@
 import ToDoItem from "./ToDoItem";
 import styles from './TodoList.module.css';
 
-export default function TodoList({ toDoList , setToDoList }) {
+export default function TodoList({ toDoList, setToDoList }) {
+
+    const sortedToDoList = toDoList
+        .slice()
+        .sort((a, b) => Number(a.done) - Number(b.done));
+
     //recorremos el array que nos llega del padre por props
     return (
         <div className={styles.todolist} >
-            { toDoList.map((item) => (
-                  <ToDoItem key={item.name} item={item} toDoList={toDoList}  setToDoList={setToDoList} />
-             ))}
+            {sortedToDoList.map((item) => (
+                <ToDoItem
+                    key={item.name}
+                    item={item}
+                    toDoList={toDoList}
+                    setToDoList={setToDoList} />
+            ))}
         </div>
     );
 }
