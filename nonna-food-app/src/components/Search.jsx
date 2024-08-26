9e24ce7ba9c827bed6ab024bd0864c11359c82e1
alfarofernando <alfarofernando@live.com>
2024-08-26 02:43:30 -0300

    Button search added & API call made

--- nonna-food-app/src/components/Search.jsx
+++ nonna-food-app/src/components/Search.jsx
@@ -1,23 +1,41 @@
 import { useEffect, useState } from "react";
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "03539d53cece40dc8ed02b29d023b710";
+
 export default function Search() {
 
     const [query, setQuery] = useState("");
+    const [triggerSearch, setTriggerSearch] = useState(false);
 
     //sintax of useEffect Hook
     useEffect(() => {
-        function demo() {
-            console.log("useEffect used");
+        if (triggerSearch) {
+            async function fetchFood() {
+                try {
+                    const reponse = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+                    const data = await reponse.json();
+                    console.log(data);
+                } catch (error) {
+                    console.error("Error fetching data", error);
+                } finally {
+                    setTriggerSearch(false);
+                }
+            }
+            fetchFood();
         }
-        demo();
-    }, [query]);
+    }, [triggerSearch, query]);
 
+    const handleSearch= ()=> {
+        setTriggerSearch(true);
+    }
     return (
         <div>
             <input type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 placeholder="Search for Nonna's Recipe" />
+            <button onClick={handleSearch}>Search</button>
         </div>
     );
 }
