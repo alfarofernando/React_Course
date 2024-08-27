abd0b9cc94417cdf3ee47fa61fab4594fb40d7bb
AlfaDevelooper <alfarofernando@live.com>
2024-08-26 22:34:41 -0300

    Outer Container component

--- nonna-food-app/src/App.jsx
+++ nonna-food-app/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
+import Container from "./components/Container";
 
 export default function App() {
-
-const [foodData,setFoodData] = useState([]);
+  const [foodData, setFoodData] = useState([]);
 
   return (
     <div className="App">
       <Nav />
-      <Search foodData={foodData} setFoodData={setFoodData}  />
-      <FoodList foodData={foodData} setFoodData={setFoodData} />
-      </div>
-  )
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <FoodList foodData={foodData} setFoodData={setFoodData} />
+      </Container>
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </div>
+  );
 }
--- nonna-food-app/src/components/Search.jsx
+++ nonna-food-app/src/components/Search.jsx
@@ -4,43 +4,46 @@ import styles from "./Search.module.css";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "03539d53cece40dc8ed02b29d023b710";
 
-export default function Search({foodData,setFoodData}) {
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("");
+  const [triggerSearch, setTriggerSearch] = useState(false);
 
-    const [query, setQuery] = useState("");
-    const [triggerSearch, setTriggerSearch] = useState(false);
-
-    //sintax of useEffect Hook
-    useEffect(() => {
-        if (triggerSearch) {
-            async function fetchFood() {
-                try {
-                    const reponse = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
-                    const data = await reponse.json();
-                    setFoodData(data.results);
-                } catch (error) {
-                    console.error("Error fetching data", error);
-                } finally {
-                    setTriggerSearch(false);
-                }
-            }
-            fetchFood();
+  //sintax of useEffect Hook
+  useEffect(() => {
+    if (triggerSearch) {
+      async function fetchFood() {
+        try {
+          console.log("search Triggered");
+          const reponse = await fetch(
+            `${URL}?query=${query}&apiKey=${API_KEY}`
+          );
+          const data = await reponse.json();
+          setFoodData(data.results);
+        } catch (error) {
+          console.error("Error fetching data", error);
+        } finally {
+          setTriggerSearch(false);
         }
-    }, [triggerSearch, query]);
-
-    const handleSearch= ()=> {
-        setTriggerSearch(true);
+      }
+      fetchFood();
     }
-    return (
-        <div className={styles.searchContainer}>
-            <input
-                className={styles.input} 
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Search in Nonna's Recipes' Grandbook" />
-            <button
-                className={styles.button} 
-                onClick={handleSearch}>Search</button>
-        </div>
-    );
+  }, [triggerSearch, query]);
+
+  const handleSearch = () => {
+    setTriggerSearch(true);
+  };
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search in Nonna's Recipes' Grandbook"
+      />
+      <button className={styles.button} onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
