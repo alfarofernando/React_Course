df897f3e91d466e2b4cc1d0ff68d1073684098b2
alfarofernando <alfarofernando@live.com>
2024-08-28 07:22:21 -0300

    v62 fetching ingredients

--- nonna-food-app/src/components/FoodDetails.jsx
+++ nonna-food-app/src/components/FoodDetails.jsx
@@ -4,7 +4,7 @@ import styles from "./FoodDetails.module.css";
 export default function FoodDetails({ foodId }) {
 
     const [food, setFood] = useState({});
-    const [isLoading,setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
     const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
     const API_KEY = "03539d53cece40dc8ed02b29d023b710";
@@ -24,7 +24,7 @@ export default function FoodDetails({ foodId }) {
     }, [foodId])
 
     return (
-        
+
         <div className={styles.recipeMain}>
             <div className={styles.recipeCard}>
                 <h1 className={styles.recipeName}>{food.title}</h1>
@@ -46,16 +46,32 @@ export default function FoodDetails({ foodId }) {
                 <div>
                     💲 <span>{(food.pricePerServing / 100).toFixed(2)} Per Serving</span>
                 </div>
-            
-            <div className={styles.recipeInstructions}>
-                <h2>Instructions</h2>
-                {isLoading? <p>Loading...</p> : 
-                <ol>
-                    {food.analyzedInstructions[0].steps.map((step,index) => (
-                        <li key={index}>{step.step}</li>
-                    ))}
-                </ol>}
-            </div>
+
+                <div className={styles.recipeInstructions}>
+                    <h2>Ingredients</h2>
+                    {isLoading ? <p>Select recipe to load ingredients...</p> :
+                        <ul>
+                            {food.extendedIngredients.map((ingredient,index) => (
+                                <div key={index}>
+                                    <img src={`https://spoonacular.com/cdn/ingredients_100x100/` + ingredient.image} alt="imagen de ingrediente" />
+                                    <h3>{ingredient.name}</h3>
+                                    <h3>${ingredient.amount}${ingredient.unit}</h3>
+                                </div>
+                            ))}
+                        </ul>
+                    }
+                </div>
+
+
+                <div className={styles.recipeInstructions}>
+                    <h2>Instructions</h2>
+                    {isLoading ? <p>Select recipe to load instuctions...</p> :
+                        <ol>
+                            {food.analyzedInstructions[0].steps.map((step, index) => (
+                                <li key={index}>{step.step}</li>
+                            ))}
+                        </ol>}
+                </div>
             </div>
         </div>
     );
