49d307e0f9286bd4b9e3436d5397dfa3f4bb5c44
alfarofernando <alfarofernando@live.com>
2024-08-28 05:40:49 -0300

    v60 fetching recipe instructions

--- nonna-food-app/src/components/FoodDetails.jsx
+++ nonna-food-app/src/components/FoodDetails.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function FoodDetails({ foodId }) {
 
     const [food, setFood] = useState({});
+    const [isLoading,setIsLoading] = useState(true);
 
     const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
     const API_KEY = "03539d53cece40dc8ed02b29d023b710";
@@ -15,15 +16,45 @@ export default function FoodDetails({ foodId }) {
                 const data = await response.json();
                 console.log(data);
                 setFood(data)
+                setIsLoading(false);
             }
             fetchFood()
         }
     }, [foodId])
 
     return (
+        
         <div>
-            <h1>{food.title}</h1>
-            <img src={food.image} />
+            <div>
+                <h1>{food.title}</h1>
+                <img src={food.image} />
+                <div>
+                    <span>
+                        ⏰<strong>{food.readyInMinutes} Minutes</strong>
+                    </span>
+                    <span>
+                        🍽<strong> Serves {food.servings}</strong>
+                    </span>
+                    <span>
+                        {food.vegetarian ? "🥕 Vegetarian" : "🍖 Non-Vegetarian"}
+                    </span>
+                    <span>
+                        {food.vegan ? "🐮 Vegan" : ""}
+                    </span>
+                </div>
+                <div>
+                    💲 <span>{(food.pricePerServing / 100).toFixed(2)} Per Serving</span>
+                </div>
+            </div>
+            <div>
+                <h2>Instructions</h2>
+                {isLoading? <p>Loading...</p> : 
+                <ol>
+                    {food.analyzedInstructions[0].steps.map((step,index) => (
+                        <li key={index}>{step.step}</li>
+                    ))}
+                </ol>}
+            </div>
         </div>
     );
 }
